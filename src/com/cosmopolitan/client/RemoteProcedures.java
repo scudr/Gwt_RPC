e57c8e62b50f0ec064393332e3dfbb264e2e460f
Carlos <cajuesca13@hotmail.com>
2015-08-02 11:42:12 -0500

    Initial commit

---
+++ src/com/cosmopolitan/client/RemoteProcedures.java
@@ -0,0 +1,15 @@
+package com.cosmopolitan.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class RemoteProcedures implements EntryPoint {
+	/**
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+	}
+}
